7918b2a41f712ed1292e35e1f25ec75b438482e8
Jose SR <josesanchez09190@gmail.com>
2024-10-07 21:10:26 -0600

    upgraded table users

--- src/main/java/com/jsrdev/TasksAPI/domain/user/UserResponse.java
+++ src/main/java/com/jsrdev/TasksAPI/domain/user/UserResponse.java
@@ -3,9 +3,10 @@ package com.jsrdev.TasksAPI.domain.user;
 public record UserResponse(
         Long id,
         String login,
-        UserRole role
+        UserRole role,
+        Boolean active
 ) {
     public UserResponse(User user) {
-        this(user.getId(), user.getLogin(), user.getRole());
+        this(user.getId(), user.getLogin(), user.getRole(), user.getActive());
     }
 }
